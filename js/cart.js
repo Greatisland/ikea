d4b70eab0fe67737aede24bac4a05b9ed34f89bb
Hyeon JIN <dontsendloveletters@gmail.com>
2023-04-03 07:43:25 +0000

    경로 수정

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,11 @@
-import wishList from "./wishList.js"
-import modalCart from "./modalCart.js"
-
 let shoppingSec = document.querySelector(".shopping_basket"), //장바구니 섹션
-itemAdds = document.querySelectorAll(".bag_btn"), // 각 item에 위치한 장바구니 추가 버튼
-productList = shoppingSec.querySelector(".product_add"), //추가한 item이 담기는 부모 ul요소
-resultPrice = shoppingSec.querySelector(".result .price"), // 장바구니 금액 렌더링
-deliveryCharge = 29000, // 배송비
-deliveryState = false // 배송비 추가유무 상태변수 (배송비는 1회만 추가되어야 함)
+  itemAdds = document.querySelectorAll(".bag_btn"), // 각 item에 위치한 장바구니 추가 버튼
+  productList = shoppingSec.querySelector(".product_add"), //추가한 item이 담기는 부모 ul요소
+  resultPrice = shoppingSec.querySelector(".result .price"), // 장바구니 금액 렌더링
+  shoppingBtn = document.querySelector(".bag_icon"), // header에 위치한 장바구니창 열림 버튼
+  couponBtn = shoppingSec.querySelector(".coupon .title"),
+  deliveryCharge = 29000, // 배송비
+  deliveryState = false // 배송비 추가유무 상태변수 (배송비는 1회만 추가되어야 함)
 
 
 let state = {
@@ -123,5 +122,23 @@ productList.addEventListener("click", (e) => {
   } //장바구니 item 갯수 빼기
 })
 
-modalCart()
-wishList()
+couponBtn.addEventListener("click", () => {
+  document.coupon.classList.toggle("on")
+  })
+
+  shoppingBtn.addEventListener("click", () => {
+  shoppingSec.classList.toggle("on")
+  if (shoppingSec.classList.contains("on")) {
+    document.querySelector(".wish_list").classList.remove("on")
+  }
+  })
+  document
+  .querySelector(".shopping_basket .exit")
+  .addEventListener("click", () => {
+    shoppingSec.classList.remove("on")
+  })
+  document
+  .querySelector(".shopping_basket .back_btn")
+  .addEventListener("click", () => {
+    shoppingSec.classList.remove("on")
+})
